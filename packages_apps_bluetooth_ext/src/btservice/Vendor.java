b73df181cf6f54395e0d45b125c1398cfffeb1da
Srinu Jella <sjella@codeaurora.org>
2017-07-28 14:22:39 +0530

    Bluetooth: Add stack support for TWS Plus feature (2)
    - Add support for parsing the TWS sink SDP record
      for TWS.
    
    - Introduced a new vendor callback to update TWS
      related properties.
    
    - As part of turn on it will register with TWS src
      record and same will de-registered as part of turn
      off.
    
    - Internal APIs are provided to know the device is
      TWS device and to know peer TWS device address.
    
    - It maintains the TWS device info at conf file where
      bonded info is stored
    
    CRs Fixed: 2188075
    Change-Id: I82da5150a9e32a5fd9acfe369a70a89df8371e27

--- packages_apps_bluetooth_ext/src/btservice/Vendor.java
+++ packages_apps_bluetooth_ext/src/btservice/Vendor.java
@@ -1,5 +1,39 @@
 /*
  * Copyright (C) 2016-2017 The Linux Foundation. All rights reserved
+ * Not a Contribution.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ * * Neither the name of The Linux Foundation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -98,6 +132,44 @@ final class Vendor {
         //intent.putExtra(BluetoothDevice.EXTRA_RSSI, rssi);
         //intent.putExtra(BluetoothDevice.EXTRA_LINK_QUALITY, linkQuality);
         //mService.sendBroadcast(intent, AdapterService.BLUETOOTH_PERM);
+    }
+    void devicePropertyChangedCallback(byte[] address, int[] types, byte[][] values) {
+        byte[] val;
+        int type;
+        short twsPlusType;
+        boolean autoConnect;
+        byte[] mPeerAddress;
+        if (types.length <= 0) {
+            Log.e(TAG, "No properties to update");
+            return;
+        }
+
+        for (int j = 0; j < types.length; j++) {
+            type = types[j];
+            val = values[j];
+            if (val.length > 0) {
+                Log.d(TAG, "Property type: " + type);
+                switch (type) {
+                    case AbstractionLayer.BT_VENDOR_PROPERTY_TWS_PLUS_DEVICE_TYPE:
+                        twsPlusType = val[0];
+                        mService.setTwsPlusDevType(address, twsPlusType);
+                        //debugLog("Remote Device name is: " + device.mName);
+                        break;
+                    case AbstractionLayer.BT_VENDOR_PROPERTY_TWS_PLUS_PEER_ADDR:
+                        mPeerAddress = val;
+                        mService.setTwsPlusPeerEbAddress(address, mPeerAddress);
+                        break;
+                    case AbstractionLayer.BT_VENDOR_PROPERTY_TWS_PLUS_AUTO_CONNECT:
+                        if(val[0] > 0) {
+                          autoConnect = true;
+                        } else {
+                          autoConnect = false;
+                        }
+                        mService.setTwsPlusAutoConnect(address, autoConnect);
+                        break;
+                }
+            }
+        }
     }
     private native void bredrcleanupNative();
     private native void initNative();
