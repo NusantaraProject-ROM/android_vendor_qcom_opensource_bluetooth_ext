6f3d7d163f3406b0e3111fbd1fa8c28f8ab74c9e
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2018-05-02 13:36:03 +0530

    Serialize resource acqisition to avoid deadlock.
    ANR is occuring due to below deadlock.
    buildBrowsablePlayerList() -->acquires mBrowsePlayerInfoList
          ->getMediaPlayerInfo(packageName) -->waiting for Avrcp.this
    onActiveSessionsChanged() -->acquires  Avrcp.this
          ->isBrowseSupported -->waiting for mBrowsePlayerInfoList
    
    To avoid this ANR serailize resource acqisition.
    
    CRs-Fixed: 2239501
    Change-Id: I7f29f5ba1052f10a98c6ed65e1ab7cb900972cd8

--- packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
+++ packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
@@ -3055,13 +3055,15 @@ public final class Avrcp_ext {
 
     private void removePackageFromBrowseList(String packageName) {
         if (DEBUG) Log.d(TAG, "removePackageFromBrowseList: " + packageName);
-        synchronized (mBrowsePlayerInfoList) {
-            int browseInfoID = getBrowseId(packageName);
-            if (browseInfoID != -1) {
-                mBrowsePlayerInfoList.remove(browseInfoID);
+        synchronized (this) {
+            synchronized (mBrowsePlayerInfoList) {
+                int browseInfoID = getBrowseId(packageName);
+                if (browseInfoID != -1) {
+                    mBrowsePlayerInfoList.remove(browseInfoID);
+                }
             }
         }
-         Log.d(TAG, "Exit removePackageFromBrowseList");
+        Log.d(TAG, "Exit removePackageFromBrowseList");
     }
 
     /*
@@ -3071,13 +3073,15 @@ public final class Avrcp_ext {
     private int getBrowseId(String packageName) {
         boolean response = false;
         int browseInfoID = 0;
-        synchronized (mBrowsePlayerInfoList) {
-            for (BrowsePlayerInfo info : mBrowsePlayerInfoList) {
-                if (info.packageName.equals(packageName)) {
-                    response = true;
-                    break;
+        synchronized (this) {
+            synchronized (mBrowsePlayerInfoList) {
+                for (BrowsePlayerInfo info : mBrowsePlayerInfoList) {
+                    if (info.packageName.equals(packageName)) {
+                        response = true;
+                        break;
+                    }
+                    browseInfoID++;
                 }
-                browseInfoID++;
             }
         }
 
@@ -3310,10 +3314,12 @@ public final class Avrcp_ext {
         String browseServiceName = "";
 
         // getting the browse service name from browse player info
-        synchronized (mBrowsePlayerInfoList) {
-            int browseInfoID = getBrowseId(packageName);
-            if (browseInfoID != -1) {
-                browseServiceName = mBrowsePlayerInfoList.get(browseInfoID).serviceClass;
+        synchronized (this) {
+            synchronized (mBrowsePlayerInfoList) {
+                int browseInfoID = getBrowseId(packageName);
+                if (browseInfoID != -1) {
+                    browseServiceName = mBrowsePlayerInfoList.get(browseInfoID).serviceClass;
+                }
             }
         }
 
@@ -3323,32 +3329,34 @@ public final class Avrcp_ext {
     }
 
     void buildBrowsablePlayerList() {
-        synchronized (mBrowsePlayerInfoList) {
-            mBrowsePlayerInfoList.clear();
-            Intent intent = new Intent(android.service.media.MediaBrowserService.SERVICE_INTERFACE);
-            List<ResolveInfo> playerList =
-                    mPackageManager.queryIntentServices(intent, PackageManager.MATCH_ALL);
+        synchronized (this) {
+            synchronized (mBrowsePlayerInfoList) {
+                mBrowsePlayerInfoList.clear();
+                Intent intent = new Intent(android.service.media.MediaBrowserService.SERVICE_INTERFACE);
+                List<ResolveInfo> playerList =
+                        mPackageManager.queryIntentServices(intent, PackageManager.MATCH_ALL);
 
-            for (ResolveInfo info : playerList) {
-                String displayableName = info.loadLabel(mPackageManager).toString();
-                String serviceName = info.serviceInfo.name;
-                String packageName = info.serviceInfo.packageName;
+                for (ResolveInfo info : playerList) {
+                    String displayableName = info.loadLabel(mPackageManager).toString();
+                    String serviceName = info.serviceInfo.name;
+                    String packageName = info.serviceInfo.packageName;
 
-                if (DEBUG) Log.d(TAG, "Adding " + serviceName + " to list of browsable players");
-                BrowsePlayerInfo currentPlayer =
-                        new BrowsePlayerInfo(packageName, displayableName, serviceName);
-                mBrowsePlayerInfoList.add(currentPlayer);
-                MediaPlayerInfo playerInfo = getMediaPlayerInfo(packageName);
-                MediaController controller =
-                        (playerInfo == null) ? null : playerInfo.getMediaController();
-                // Refresh the media player entry so it notices we can browse
-                if (controller != null) {
-                    addMediaPlayerController(controller.getWrappedInstance());
-                } else {
-                    addMediaPlayerPackage(packageName);
+                    if (DEBUG) Log.d(TAG, "Adding " + serviceName + " to list of browsable players");
+                    BrowsePlayerInfo currentPlayer =
+                            new BrowsePlayerInfo(packageName, displayableName, serviceName);
+                    mBrowsePlayerInfoList.add(currentPlayer);
+                    MediaPlayerInfo playerInfo = getMediaPlayerInfo(packageName);
+                    MediaController controller =
+                            (playerInfo == null) ? null : playerInfo.getMediaController();
+                    // Refresh the media player entry so it notices we can browse
+                    if (controller != null) {
+                        addMediaPlayerController(controller.getWrappedInstance());
+                    } else {
+                        addMediaPlayerPackage(packageName);
+                    }
                 }
+                updateCurrentMediaState(null);
             }
-            updateCurrentMediaState(null);
         }
     }
 
@@ -3588,12 +3596,14 @@ public final class Avrcp_ext {
      * @return true if it supports browsing, else false.
      */
     private boolean isBrowseSupported(String packageName) {
-        synchronized (mBrowsePlayerInfoList) {
-            /* check if Browsable Player's list contains this package name */
-            for (BrowsePlayerInfo info : mBrowsePlayerInfoList) {
-                if (info.packageName.equals(packageName)) {
-                    if (DEBUG) Log.v(TAG, "isBrowseSupported for " + packageName + ": true");
-                    return true;
+        synchronized (this) {
+            synchronized (mBrowsePlayerInfoList) {
+                /* check if Browsable Player's list contains this package name */
+                for (BrowsePlayerInfo info : mBrowsePlayerInfoList) {
+                    if (info.packageName.equals(packageName)) {
+                        if (DEBUG) Log.v(TAG, "isBrowseSupported for " + packageName + ": true");
+                        return true;
+                    }
                 }
             }
         }
