45e25a3ab52cea45138c0c8854bdac0756040444
Naval saini <saini@codeaurora.org>
2018-05-14 14:39:09 +0530

    Avrcp: Sending available player CHANGED response during registration
    Earlier when BT turns ON then mAvailablePlayerviewchanged flag is set true,
    and because of this when we send A2DP START or AVRCP play from DUT
    then in first attempt CHANGED response gets ignore and we retured
    without updating current play status to remote.
    Now we have shifted this logic to available player changed event,
    where this flag will be set as false during registration.
    
    Change-Id: I709527ab6667e9b61581471894a526ba59915155
    CRs-Fixed: 2238114

--- packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
+++ packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
@@ -1838,19 +1838,6 @@ public final class Avrcp_ext {
                             + currentAttributes.toRedactedString() + " : "
                             + mMediaAttributes.toRedactedString());
 
-
-            if (mAvailablePlayerViewChanged && addr != null &&
-                    index != INVALID_DEVICE_INDEX &&
-                    (deviceFeatures[index].mAvailablePlayersChangedNT ==
-                        AvrcpConstants.NOTIFICATION_TYPE_INTERIM)) {
-                Log.v(TAG, "Sending response for available playerchanged:");
-                deviceFeatures[index].mAvailablePlayersChangedNT =
-                                   AvrcpConstants.NOTIFICATION_TYPE_CHANGED;
-                registerNotificationRspAvalPlayerChangedNative(
-                        AvrcpConstants.NOTIFICATION_TYPE_CHANGED, addr);
-                mAvailablePlayerViewChanged = false;
-                return;
-            }
             if (addr != null && mReportedPlayerID != mCurrAddrPlayerID &&
                     index != INVALID_DEVICE_INDEX) {
                 if (deviceFeatures[index].mAvailablePlayersChangedNT ==
@@ -2096,6 +2083,17 @@ public final class Avrcp_ext {
                 registerNotificationRspAvalPlayerChangedNative(
                         AvrcpConstants.NOTIFICATION_TYPE_INTERIM,
                         getByteAddress(deviceFeatures[deviceIndex].mCurrentDevice));
+                if (mAvailablePlayerViewChanged &&
+                        (deviceFeatures[deviceIndex].mAvailablePlayersChangedNT ==
+                        AvrcpConstants.NOTIFICATION_TYPE_INTERIM)) {
+                    Log.v(TAG, "Sending response for available playerchanged:");
+                    deviceFeatures[deviceIndex].mAvailablePlayersChangedNT =
+                                                 AvrcpConstants.NOTIFICATION_TYPE_CHANGED;
+                    registerNotificationRspAvalPlayerChangedNative(
+                            AvrcpConstants.NOTIFICATION_TYPE_CHANGED,
+                            getByteAddress(deviceFeatures[deviceIndex].mCurrentDevice));
+                    mAvailablePlayerViewChanged = false;
+                }
                 break;
 
             case EVT_ADDR_PLAYER_CHANGED:
