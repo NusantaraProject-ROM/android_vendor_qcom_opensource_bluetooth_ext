0dc29e739344b3d52d169c5c77106dca70dfd818
Sumit Deshmukh <sumitd@codeaurora.org>
2018-07-03 11:09:34 +0530

    MAP: [IOT] Update MAS Email folder name case to small letters
    Usecase:
    1) Establish MAP MAS EMAIL session.
    2) Send message from carkit and wait for sending success
       event report.
    3) Observe if Email in carkit is updated as sent or not.
    4) Similar case with read status.
    
    Issue:
    Email in carkit is not updated as sent.
    
    Fix:
    This change updates case of mandatory folder name of MAS Email
    account.
    
    Root Cause:
    Some remote carkits is case sensitive towards folder
    name. So folder name sent in Capital letters is not processed
    by this carkit as case of foldername in Folderlisting and Event
    Report is not same (telecom/msg/sent for FolderListing and
    telecom/msg/SENT for EventReport).
    
    Change-Id: I805c00ebd1e3e4974f8d4a92040a4e124d9f3722
    CRs-Fixed: 2272160

--- packages_apps_bluetooth_ext/src/map/lib/BluetoothMapEmailContract.java
+++ packages_apps_bluetooth_ext/src/map/lib/BluetoothMapEmailContract.java
@@ -176,13 +176,13 @@ public final class BluetoothMapEmailContract {
      * E.g. as a mapping for them such that the naming will match the underlying
      * matching folder ID's.
      */
-    public static final String FOLDER_NAME_INBOX   = "INBOX";
-    public static final String FOLDER_NAME_SENT    = "SENT";
-    public static final String FOLDER_NAME_OUTBOX  = "OUTBOX";
-    public static final String FOLDER_NAME_DRAFT   = "DRAFT";
-    public static final String FOLDER_NAME_DRAFTS  = "DRAFTS";
-    public static final String FOLDER_NAME_DELETED = "DELETED";
-    public static final String FOLDER_NAME_OTHER   = "OTHER";
+    public static final String FOLDER_NAME_INBOX   = "inbox";
+    public static final String FOLDER_NAME_SENT    = "sent";
+    public static final String FOLDER_NAME_OUTBOX  = "outbox";
+    public static final String FOLDER_NAME_DRAFT   = "draft";
+    public static final String FOLDER_NAME_DRAFTS  = "drafts";
+    public static final String FOLDER_NAME_DELETED = "deleted";
+    public static final String FOLDER_NAME_OTHER   = "other";
 
     /**
      * Folder IDs to be used with Instant Messaging virtual folders
