6d142fc80e547e7eb887f48876f75a9b89b75064
pramod kotreshappa <kpramod@codeaurora.org>
2019-01-11 16:04:19 -0800

    TWS_AVRCP: Fix for volume change on streaming earbud
    Fix for volume changed to safe volume on streaming earbud when
    second earbud is connected.
    
    Change-Id: I5f7e1514ace248324c6a4753b8b29bfd4c95f441

--- packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
+++ packages_apps_bluetooth_ext/src/avrcp/Avrcp_ext.java
@@ -3098,7 +3098,8 @@ public final class Avrcp_ext {
              (mDevice.isTwsPlusDevice() && device.isTwsPlusDevice()))) {
             setActiveDevice(mDevice);
             //below line to send setAbsolute volume if device is suporting absolute volume
-            setAbsVolumeFlag(mDevice);
+            if (mDevice.equals(deviceFeatures[index].mCurrentDevice))
+                setAbsVolumeFlag(mDevice);//Do not call this funciton for second EB connect
             //When A2dp playing on DUT and Remote got connected, send proper playstatus
             if (isPlayingState(mCurrentPlayerState) &&
                 mA2dpService.isA2dpPlaying(device)) {
