a97744cadf63d9bb7d644f591bb82f39da51d3a1
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2019-02-18 17:55:15 +0530

    TWS: Enable/disable TWS service with bredr services(2/2)
    - Enable/disable TWS service with bredr services
    
    - Remove TWS service enable from vendor init
      TWS service won't work in BLE on use case if tws
      enable alligned to vendor init
    
    Change-Id: I384a62f60e1c49db1724ea467efcf65931d342a0
    CRs-Fixed: 2401276

--- system_bt_ext/btif/src/btif_vendor.cc
+++ system_bt_ext/btif/src/btif_vendor.cc
@@ -146,7 +146,6 @@ static bt_status_t init( btvendor_callbacks_t* callbacks)
     broadcast_cb_timer = alarm_new("btif_vnd.cb_timer");
     LOG_INFO(LOG_TAG,"init");
     LOG_INFO(LOG_TAG,"init done");
-    btif_enable_service(BTA_TWS_PLUS_SERVICE_ID);
     return BT_STATUS_SUCCESS;
 }
 
