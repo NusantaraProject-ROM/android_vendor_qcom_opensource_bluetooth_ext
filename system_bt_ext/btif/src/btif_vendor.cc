bba81695bad0d7d5b5c946e96cbc5b83a0680782
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2019-08-29 16:55:06 +0530

    GAP: Don't block main thread for hci close
    When timeouts happened at profile level, APP
    triggers hci close and after hci close completion
    APP triggers process kill.
    But if any HAL taking more time to process close
    profiles may triggers random Exceptions.
    
    Hence send close to hal and return mainthread.
    
    Change-Id: Ib8fa5779c37c0b067c1ca616334908d059727017
    CRs-Fixed: 2517612

--- system_bt_ext/btif/src/btif_vendor.cc
+++ system_bt_ext/btif/src/btif_vendor.cc
@@ -91,6 +91,7 @@
 #define BTA_SERVICE_ID_TO_SERVICE_MASK(id)  ((tBTA_SERVICE_MASK)1 << (id))
 #define CALLBACK_TIMER_PERIOD_MS      (60000)
 #define BTIF_VENDOR_BREDR_CLEANUP 1
+#define BTIF_VENDOR_HCI_CLOSE 2
 
 typedef struct {
   RawAddress bd_addr; /* BD address peer device. */
@@ -173,6 +174,10 @@ static void btif_vendor_bredr_cleanup_event(uint16_t event, char *p_param)
     HAL_CBACK(bt_vendor_callbacks, bredr_cleanup_cb, true);
 }
 
+static void btif_vendor_hci_close_event(uint16_t event, char *p_param)
+{
+    btif_hci_close();
+}
 static void btif_vendor_send_iot_info_cb(uint16_t event, char *p_param)
 {
     broadcast_cb_data.is_valid = false;
@@ -354,7 +359,8 @@ static void bredrcleanup(void)
 static void hciclose(void)
 {
     LOG_INFO(LOG_TAG,"hciclose");
-    btif_hci_close();
+    btif_transfer_context(btif_vendor_hci_close_event,BTIF_VENDOR_HCI_CLOSE,
+                          NULL, 0, NULL);
 }
 
 
